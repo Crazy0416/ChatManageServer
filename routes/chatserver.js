84693984b6a4db8a2d2d1716fb1a74f4a718db5b
Minho_Kim <minmin0416@naver.com>
2018-02-09 14:51:56 +0900

    [Fix] Fix error

--- routes/chatserver.js
+++ routes/chatserver.js
@@ -6,13 +6,18 @@ var redisClient = redis.createClient();
 
 /* GET users listing. */
 router.get('/count', function(req, res, next) {
+    var tagName = req.cookie.tagName;
+
     redisClient.select(2, function(err) {
       if (err){
         console.log("Redis select err!! : " + err);
-        res.status(500).json({
-            success: false,
-            message: "DB ERR"
-        });
+        res.status(500).append("Access-Control-Allow-Origin", "*")
+            .append("Access-Control-Allow-Headers", "origin, x-requested-with, content-type, accept")
+            .set()
+            .json({
+                success: false,
+                message: "DB ERR"
+            });
       } else {
         redisClient.hgetall('ServerClientCnt', function(err, reply) {
           console.log("ServerClientCnt all : " + JSON.stringify(reply));
@@ -33,19 +38,28 @@ router.get('/count', function(req, res, next) {
               next();
           }, function(err){
               if (err) {
-                  res.status(500).json({
+                  res.status(500).append("Access-Control-Allow-Origin", "*")
+                      .append("Access-Control-Allow-Headers", "origin, x-requested-with, content-type, accept")
+                      .set()
+                      .json({
                       success: false,
                       message: "DB ERR"
                   });
               } else {
                   console.log("leastCnt : " + leastCnt);
                   console.log("leastKey : " + leastKey);
-                  res.json({
-                      success: true,
-                      data: {
-                          ip: leastKey
-                      }
-                  });
+                  res.append("Access-Control-Allow-Origin", "*")
+                      .append('Access-Control-Allow-Methods', "POST, GET, OPTIONS, DELETE")
+                      .append("Access-Control-Allow-Headers", "origin, x-requested-with, content-type, accept")
+                      .set()
+                      .json({
+                        success: true,
+                        data: {
+                            ip: leastKey,
+                            //TODO: tagName에 대한 번호키 값
+                            tagName: tagName
+                        }
+                      });
               }
           })
 
