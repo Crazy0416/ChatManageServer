fec2ab1b4a8b03c7b9ed8ad0f207c5df55ced540
MinHo Kim <minmin0416@naver.com>
2018-02-19 14:53:14 +0900

    [Fix] Fix syntax err

--- routes/chatserver.js
+++ routes/chatserver.js
@@ -85,15 +85,12 @@ router.get('/count', function (req, res, next) {
 });
 
 router.get('/popularChat', function(req,res){
-    var tagName = req.cookies.tagName;
-
-    resdisClient.ZREVRANGE(tagName ,0,9,'withscores',function(err,result){
+    redisClient.ZREVRANGE('PopularRoom' ,0,9,'withscores',function(err,result){
         if(err){
             sentry.message(
                 "Redis get error",
                 "GET /chatserver/popularChat",
                 {
-                    note: "tag name : "+tagName,
                     type: "Redis error"
                 }
             );
